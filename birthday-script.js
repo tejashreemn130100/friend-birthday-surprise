461c8380849a69baabc382f9fd6693b05db96ba9
tejashreemn130100 <tejashree130100@gmail.com>
2024-09-12 09:47:22 +0530

    Add files via upload

---
+++ birthday-script.js
@@ -0,0 +1,15 @@
+const body = document.body;
+
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+    body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 300);
